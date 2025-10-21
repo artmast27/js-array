37cd722a012286c8753d6cbcec42f28ecfd46253
artmast27 <artemko4819@gmail.com>
2025-10-22 01:43:02 +0200

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,7 +1,12 @@
 //Функція filterAdults приймає масив об'єктів, що представлє людей (з полями name та age),
 //вам потрібно написати код, який повертає новий масив, який містить лише тих людей, чий вік більше або дорівнює 18.
-function filterAdults(people) {
-  //Ваш код
+function sumArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
-module.exports = filterAdults;
+module.exports = sumArray;
+
